0a3d6701e7b2d77f0b3f9bae34deea78b55317eb
Molly Reed <mollyreeddesign@gmail.com>
2024-06-17 22:47:59 -0400

    feat: Added fonts and cleared boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jost } from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
 
-const inter = Inter({ subsets: ["latin"] });
+const jost = Jost({
+  subsets: ["latin"],
+  variable: "--font-jost",
+  display: "swap",
+});
+
+/* IF YOU NEED A SECOND FONT, PUT IT HERE:
+const jost = Jost({
+  subsets: ['latin'],
+  variable: '--font-jost',
+  display: 'swap',
+}) */
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={clsx(jost.variable, jost.variable)}>
+      <body>{children}</body>
     </html>
   );
 }
