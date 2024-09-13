f2cb916666a015d3957f63ce44b48ae63381bce3
Molly Reed <mollyreeddesign@gmail.com>
2024-09-13 12:15:32 +0200

    feat:Added working mailchimp forms to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import * as prismic from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText } from "@prismicio/react";
 import Link from "next/link";
-import LogoFullWhiteLg from "./LogoFullWhiteLg";
+import LogoFullWhiteLg from "@/components/LogoFullWhiteLg";
 
 export default async function Footer() {
   const client = createClient();
@@ -12,7 +12,7 @@ export default async function Footer() {
 
   return (
     <footer>
-      <section className="relative  md:h-[20rem]">
+      <section className="relative">
         {prismic.isFilled.image(settings.data.footer_background_image) && (
           <PrismicNextImage
             field={settings.data.footer_background_image}
@@ -21,86 +21,100 @@ export default async function Footer() {
             className="pointer-events-none select-none object-cover -z-50 md:block"
           />
         )}
-        <div className="text-white">
-          {/*MAILCHIMP FORM BELOW, CONVERTED JSX FROM MAILCHIMP GENERATED HTML EMBED FORM*/}
-          <div className="text-center font-body">
+
+        <div className="text-white grid grid-cols-1 place-items-center text-center font-body p-10">
+          <div className="pb-2">
             <>{settings.data.footer_form_label}</>
-            <div id="mc_embed_shell" className="pt-10">
-              <div id="mc_embed_signup">
-                <form
-                  action="https://valeriejurado.us2.list-manage.com/subscribe/post?u=4a1e985219bc367027066386a&id=a1fd1262ff&f_id=00836de0f0"
-                  method="post"
-                  id="mc-embedded-subscribe-form"
-                  name="mc-embedded-subscribe-form"
-                  className="validate"
-                  target="_self"
-                >
-                  <div id="mc_embed_signup_scroll">
-                    <div className="mc-field-group">
-                      <label htmlFor="mce-EMAIL" className="hidden">
-                        Email Address
-                      </label>
-                      <input
-                        type="email"
-                        name="EMAIL"
-                        className="required email h-[40px] border border-white p-4 bg-black text-white"
-                        id="mce-EMAIL"
-                        defaultValue=""
-                      />
-                    </div>
-                    <div>
-                      <input type="hidden" name="tags" defaultValue={1538740} />
-                    </div>
-                    <div id="mce-responses" className="clear">
-                      <div
-                        className="response hidden"
-                        id="mce-error-response"
-                      />
-                      <div
-                        className="response hidden"
-                        id="mce-success-response"
-                      />
-                    </div>
-                    <div
-                      className="absolute hidden pl-[-5000px]"
-                      aria-hidden="true"
-                    >
-                      <input
-                        type="text"
-                        name="b_4a1e985219bc367027066386a_a1fd1262ff"
-                        tabIndex={-1}
-                        defaultValue=""
-                      />
-                    </div>
-                    <div className="clear text-white">
-                      <input
-                        type="submit"
-                        name="subscribe"
-                        id="mc-embedded-subscribe"
-                        className="button"
-                        defaultValue="Sign Up"
-                      />
-                    </div>
+          </div>
+          {/*SECOND MAILCHIMP FORM BELOW, CONVERTED JSX FROM MAILCHIMP GENERATED HTML EMBED FORM*/}
+          <div id="mc_embed_shell" className="">
+            <div id="mc_embed_signup">
+              <form
+                action="https://valeriejurado.us2.list-manage.com/subscribe/post?u=4a1e985219bc367027066386a&id=a1fd1262ff&f_id=00b36de0f0"
+                method="post"
+                id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                className="validate"
+                target="_self"
+                /*noValidate=""*/
+              >
+                <div id="mc_embed_signup_scroll">
+                  {/*<div className="indicates-required">
+                    <span className="asterisk">*</span> indicates required
+                  </div>*/}
+                  <div className="mc-field-group">
+                    {/*<label htmlFor="mce-EMAIL">
+                      Email Address <span className="asterisk">*</span>
+                    </label>*/}
+                    <input
+                      type="email"
+                      name="EMAIL"
+                      aria-label="email address"
+                      placeholder="Enter your email address"
+                      className="required email h-[50px] w-[280px] border border-white p-4 bg-black text-white"
+                      id="mce-EMAIL"
+                      /*required=""*/
+                      defaultValue=""
+                    />
                   </div>
-                </form>
-              </div>
+                  <div className="hidden">
+                    <input type="hidden" name="tags" defaultValue={1538740} />
+                  </div>
+                  <div id="mce-responses" className="clear">
+                    <div
+                      className="response hidden"
+                      id="mce-error-response"
+                      /*style={{ display: "none" }}*/
+                    />
+                    <div
+                      className="response hidden"
+                      id="mce-success-response"
+                      /*style={{ display: "none" }}*/
+                    />
+                  </div>
+                  <div
+                    aria-hidden="true"
+                    className="absolute left-[-5000px]"
+                    /*style={{ position: "absolute", left: "-5000px" }}*/
+                  >
+                    <input
+                      type="text"
+                      name="b_4a1e985219bc367027066386a_a1fd1262ff"
+                      tabIndex={-1}
+                      defaultValue=""
+                    />
+                  </div>
+                  <div className="font-body clear text-white md:text-xl pt-2">
+                    <input
+                      type="submit"
+                      name="subscribe"
+                      id="mc-embedded-subscribe"
+                      className="button underline underline-offset-4"
+                      defaultValue="sign up"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div className="pt-8 space-y-6">
+            <div className="py-6">
+              <Link href="/">
+                <LogoFullWhiteLg />
+              </Link>
             </div>
 
-            <Link href="/">
-              <LogoFullWhiteLg />
-            </Link>
-
-            <p>
-              © {new Date().getFullYear()} {settings.data.site_title}
-            </p>
-
-            <ul>
+            <ul className="underline underline-offset-4 md:text-xl">
               {settings.data.navigation.map(({ link, label }) => (
                 <li key={label}>
                   <PrismicNextLink field={link}>{label}</PrismicNextLink>
                 </li>
               ))}
             </ul>
+            <p>
+              © {new Date().getFullYear()} {settings.data.site_title}
+            </p>
           </div>
         </div>
       </section>
